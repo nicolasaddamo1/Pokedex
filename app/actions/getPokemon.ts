787546a17d73f6594bad6795c652f4401b088c1a
Nico <nicolasaddamo1@gmail.com>
2024-09-06 14:29:15 -0300

    getPokemon added and midified

--- app/actions/getPokemon.ts
+++ app/actions/getPokemon.ts
@@ -9,7 +9,24 @@ export async function getPokemon({
     limit?: number;
 }) {
     let apiUrl = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${(page - 1) * 24}`;
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
-    const data = await response.json();
-    return data;
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (query){
+            const filteredPokemon = data.results.filter((pokemon:{name: string}) => 
+                pokemonNameStartsWithQuery(pokemon.name, query.toLowerCase())
+        );
+        return filteredPokemon.slice(0,24);
+        }else{
+            return data.results.slice(0,24);
+        }
+    }
+    catch(error){
+        console.log(error);
+        return null
+    }
+}
+
+function pokemonNameStartsWithQuery(name: string, query: string) {
+    return name.toLowerCase().startsWith(query);
 }
