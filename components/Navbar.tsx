e2cb8a3ed018395e3b55a3e7e116b1f0792f8c96
Nico <nicolasaddamo1@gmail.com>
2024-09-06 14:07:23 -0300

    navbar modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image"
+
 const Navbar = () =>{
     return (
-    <div>
-        Navbar
+    <div className="py-5">
+        <div className="max-w-[1500px] w-[90%] mx-auto flex justify-center">
+            <Image src={"./logo.svg"} width={300} height={150} alt="logo"/>
+            
+        </div>
     </div>
     )
 }
