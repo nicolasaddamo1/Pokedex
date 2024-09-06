247a375f5ab723464911f00c74332dc366046b6f
Nico <nicolasaddamo1@gmail.com>
2024-09-06 14:43:18 -0300

    search

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
 import React from 'react'
-const Home = () => {
-  return <div>Page</div>
+import { fetchPokemon } from './actions/getPokemon';
+import LoadPokemon from '@/components/LoadPokemon';
+import Search from '@/components/Search';
+const Page = async({searchParams}:{
+  searchParams:{
+    [key:string]:string | string[] | undefined
+  }
+}) => {
+  const search = typeof searchParams.search === "string"? searchParams.search : undefined;
+  const pokemon = await fetchPokemon({search});
+
+  return <div className='max-w-[1500px] w-[95%] mx-auto'>
+    <Search search={search}/>
+    <ul>
+      <LoadPokemon search={search} initialPokemon={pokemon}/>
+    </ul>
+  </div>
   ;
 }
-export default Home
+export default Page
