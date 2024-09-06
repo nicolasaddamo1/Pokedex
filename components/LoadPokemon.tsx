b3c36692cce2d4b44f1d60538da260c7b9b597e9
Nico <nicolasaddamo1@gmail.com>
2024-09-06 14:54:48 -0300

    added hooks

--- components/LoadPokemon.tsx
+++ components/LoadPokemon.tsx
@@ -1,4 +1,14 @@
-const LoadPokemon = ()  => {
+"use client"
+import { loadBindings } from "next/dist/build/swc"
+import { useEffect, useState } from "react"
+import { useInView } from "react-intersection-observer"
+import { ClipLoader } from "react-spinners"
+const LoadPokemon = ({search, initialPokemon}: {search: string | undefined, initialPokemon: Pokemon[]|undefined})  => {
+    const [pokemon, setPokemon] = useState(initialPokemon)
+    const [page, setPage] = useState(1)
+    const [loading, setLoading]= useState(false)
+
+    const {inView, ref} = useInView()
     return <div>LoadPokemon</div>
 }
 export default LoadPokemon
